9c3de9e496d91be5ad0a19aad53e20c51cecd5d2
hsolicha <hsolicha@hotmail.com>
2016-10-22 12:06:20 -0600

    Server Adaptations

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.post('/service/prograso/usuarios/nuevo', usuariosController.nuevoUsuario);
   console.log('Server listening on port: ' + port);
 });
 */
-server.listen(server_port, server_ip_address, function () {
+app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
